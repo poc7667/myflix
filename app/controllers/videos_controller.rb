98457e59a441a2779a68866de9dc8467af682659
poc7667 <poc7667@gmail.com>
2014-05-19 14:10:19 +0800

    Misc remove unused 'binding.pry'
    Signed-off-by: poc7667 <poc7667@gmail.com>

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,13 +1,11 @@
 class VideosController < ApplicationController
 
     def index
-        binding.pry
         #@categories = Category.all
     end
 
     def show
         @video = Video.find(params[:id])
-        binding.pry
     end
     
 end
